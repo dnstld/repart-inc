3a078c432ec00630644105cce4f21ebe1561c852
Denis Toledo <contato@denistoledo.com.br>
2017-05-21 17:15:14 -0300

    crating about page

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,7 +1,11 @@
-var hi = new Vivus('my-svg', {
-	type: 'scenario-sync',
-	duration: 100,
-	start: 'autostart',
-	dashGap: 100,
-	forceRender: false
-});
+;(function() {
+	var hi = new Vivus('my-svg', {
+		type: 'scenario-sync',
+		duration: 100,
+		start: 'autostart',
+		dashGap: 100,
+		forceRender: false
+	});
+
+	$('#fullpage').fullpage();
+})();
